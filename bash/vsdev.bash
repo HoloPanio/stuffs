d60cf7737e974bb637c9d8e4a7aa6c119ce40348
Jackson <30759238+HoloPanio@users.noreply.github.com>
2022-12-04 15:33:04 -0600

    Update vsdev.bash

--- bash/vsdev.bash
+++ bash/vsdev.bash
@@ -2,14 +2,14 @@
 # Written by lucemans (https://github.com/lucemans)
 # Modified by HoloPanio (https://github.com/HoloPanio)
 
-LUC_DEV_FOLDER='/home/holopanio/projects/'
+DEV_FOLDER='/home/holopanio/projects/'
 function vsdev () {
-    LUC_INPUT=$(ls $LUC_DEV_FOLDER | fzf --reverse)
-    if [[ ${#LUC_INPUT} -ne 0 ]]; then
-        LUC_INPUT2=$LUC_DEV_FOLDER$LUC_INPUT;
-        cd $LUC_INPUT2
-        code $LUC_INPUT2
-        echo -e '\e[37m======================================\e[39m\n\n    Opening project \e[36m'$LUC_INPUT'\e[39m\n\n\e[37m======================================'
+    INPUT=$(ls $LUC_DEV_FOLDER | fzf --reverse)
+    if [[ ${#INPUT} -ne 0 ]]; then
+        INPUT2=$DEV_FOLDER$INPUT;
+        cd $INPUT2
+        code $INPUT2
+        echo -e '\e[37m======================================\e[39m\n\n    Opening project \e[36m'$INPUT'\e[39m\n\n\e[37m======================================'
         #code
     fi
 }
